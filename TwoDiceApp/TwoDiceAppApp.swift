83bbcdd0792952564fc7918de4007fa5625ec9a8
vivektarun <vivektarun1234@gmail.com>
2023-07-26 19:25:55 +0530

    initial file add

---
+++ TwoDiceApp/TwoDiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoDiceAppApp.swift
+//  TwoDiceApp
+//
+//  Created by Vivek Tarun on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TwoDiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
